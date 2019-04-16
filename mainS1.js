1f12d3fb431288949d2a883d58d0d97ad02fd552
sbd584 <32025147+sbd584@users.noreply.github.com>
2019-04-15 23:51:15 -0500

    Update mainS1.js

--- mainS1.js
+++ mainS1.js
@@ -1,3 +1,3 @@
 // var game = new Phaser.Game(1200, 800, Phaser.AUTO);
-// game.state.add('intro', demo.menu);
-// game.state.start('intro');
+// game.state.add('lvl1', demo.menu);
+// game.state.start('lvl1');
